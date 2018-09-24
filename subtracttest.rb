36a75c282dba49c681b712c8d2ea7cb60c4dfab3
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-09-24 15:05:24 -0400

    fail three times

---
+++ subtracttest.rb
@@ -0,0 +1,37 @@
+require "minitest/autorun"
+require_relative "subtract.rb"
+
+class Subtract_test < Minitest::Test
+
+def test_one
+	assert_equal(20, subtract(30, 10))
+end
+def test_two
+	assert_equal(10, subtract(-5, -15))
+end
+def test_three
+	assert_equal(100, subtract(500, 400))
+end
+def test_four
+	assert_equal(-80, subtract(-280, -200))
+end
+def test_five
+	assert_equal(90, subtract(100, 10))
+end
+def test_six
+	assert_equal(10.5, subtract(10.75, 0.25))
+end
+def test_seven
+	assert_equal(20, subtract(60, 40))
+end
+def test_eight
+	assert_equal(20.25, subtract(35.75, 15.25))
+end
+def test_nine
+	assert_equal(-10.5, subtract(21, 10.5))
+end
+def test_ten
+	assert_equal(1.234, subtract(2.234, 1.5))
+end
+
+end
