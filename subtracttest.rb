765a9bf120be7a033fd2d7794bbb9c7b5ea9ba70
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-09-24 15:09:35 -0400

    pass test

--- subtracttest.rb
+++ subtracttest.rb
@@ -25,13 +25,13 @@ def test_seven
 	assert_equal(20, subtract(60, 40))
 end
 def test_eight
-	assert_equal(20.25, subtract(35.75, 15.25))
+	assert_equal(20.5, subtract(35.75, 15.25))
 end
 def test_nine
-	assert_equal(-10.5, subtract(21, 10.5))
+	assert_equal(10.5, subtract(21, 10.5))
 end
 def test_ten
-	assert_equal(1.234, subtract(2.234, 1.5))
+	assert_equal(1.234, subtract(2.234, 1))
 end
 
 end
