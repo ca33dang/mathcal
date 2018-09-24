57c732571c094c1ed3f42ff6cc788c0b8b75b5ee
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-09-24 13:37:02 -0400

    Test fail

--- addtesting.rb
+++ addtesting.rb
@@ -6,4 +6,10 @@ class Add_test < Minitest::Test
 def test_assert_num1_plus_num2_plus_num3_plus_num4_1738
 	assert_equal(1738, addition(1000,700,30,8))
 end
+def test_2
+	assert_equal(500, addition(200, 200, 50, 50))
+end
+def test_3
+	assert_equal(200, addition(100, 1, 10, 2))
+end
 end
